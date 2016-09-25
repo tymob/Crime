310b3c28d3c2667885e08f04c69db3c90653069a
Brent Campbell <brent@Brents-MacBook-Pro.local>
2016-09-25 14:49:04 -0500

    Initial Commit

---
+++ Crime Analyzer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Crime Analyzer
+//
+//  Created by Brent Campbell on 9/25/16.
+//  Copyright © 2016 Brent Campbell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
